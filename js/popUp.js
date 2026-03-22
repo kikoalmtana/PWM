604f9b18b214a8a39ba090e95c86b47cc5074f11
Ibakce <ibautevega8@gmail.com>
2026-03-22 18:05:33 +0000

    Added Pop-up for delete account

--- js/popUp.js
+++ js/popUp.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('1overlay:', document.getElementById('overlay'));
-    console.log('2openBtn:', document.getElementById('openPopup'));
-    console.log('3closeBtn:', document.getElementById('closePopup'));
-    console.log('4sendBtn:', document.getElementById('sendBtn'));
-    console.log('5formContent:', document.getElementById('formContent'));
-    console.log('6successMsg:', document.getElementById('successMsg'));
-    console.log('7inputEmail:', document.getElementById('inputEmail'));
+    console.log('overlay:', document.getElementById('overlay'));
+    console.log('openBtn:', document.getElementById('openPopup'));
+    console.log('closeBtn:', document.getElementById('closePopup'));
+    console.log('sendBtn:', document.getElementById('sendBtn'));
+    console.log('formContent:', document.getElementById('formContent'));
+    console.log('successMsg:', document.getElementById('successMsg'));
+    console.log('inputEmail:', document.getElementById('inputEmail'));
     const overlay = document.getElementById('overlay');
     const openBtn = document.getElementById('openPopup');
     const closeBtn = document.getElementById('closePopup');
@@ -14,26 +14,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const successMsg = document.getElementById('successMsg');
     const inputEmail = document.getElementById('inputEmail');
 
-    openBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        overlay.classList.add('open');
-        formContent.style.display = 'flex';
-        successMsg.style.display = 'none';
-        inputEmail.value = '';
-    });
+    if (openBtn) {
+        openBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            overlay.classList.add('open');
+            formContent.style.display = 'flex';
+            successMsg.style.display = 'none';
+            inputEmail.value = '';
+        });
+    }
 
-    closeBtn.addEventListener('click', () => overlay.classList.remove('open'));
+    if (closeBtn) {
+        closeBtn.addEventListener('click', () => overlay.classList.remove('open'));
+    }
 
-    overlay.addEventListener('click', (e) => {
-        if (e.target === overlay) overlay.classList.remove('open');
-    });
 
-    sendBtn.addEventListener('click', () => {
-        if (!inputEmail.value) {
-            inputEmail.focus();
-            return;
-        }
-        formContent.style.display = 'none';
-        successMsg.style.display = 'block';
-    });
+    if (overlay) {
+        overlay.addEventListener('click', (e) => {
+            if (e.target === overlay) overlay.classList.remove('open');
+        });
+    }
+
+    if (sendBtn){
+        sendBtn.addEventListener('click', () => {
+            if (!inputEmail.value) {
+                inputEmail.focus();
+                return;
+            }
+            formContent.style.display = 'none';
+            successMsg.style.display = 'block';
+        });
+    }
+
 });

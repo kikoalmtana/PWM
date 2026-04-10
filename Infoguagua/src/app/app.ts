5d5b1ee33343d6dbfab15109d2bec9cb8cf478af
Ibakce <ibautevega8@gmail.com>
2026-04-10 17:48:05 +0100

    Added footer in app.html

--- Infoguagua/src/app/app.ts
+++ Infoguagua/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { PassInfoComponent } from './components/pass-info/pass-info';
+import {FooterComponent} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, PassInfoComponent],
+  imports: [RouterOutlet, HeaderComponent, PassInfoComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

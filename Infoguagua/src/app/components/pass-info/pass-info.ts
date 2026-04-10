1d26d910185a0b1434be177492ee3bfff6e38877
Andres Barroso <andresbarrososoriano@gmail.com>
2026-04-10 17:37:11 +0100

    added footer and lines-warnings components

---
+++ Infoguagua/src/app/components/pass-info/pass-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pass-info',
+  imports: [],
+  templateUrl: './pass-info.html',
+  styleUrl: './pass-info.css',
+})
+export class PassInfoComponent {
+
+}

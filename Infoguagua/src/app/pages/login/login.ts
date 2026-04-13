f8768e8d9449670d5b5e9b33cab5b832b7328e8e
Ibakce <ibautevega8@gmail.com>
2026-04-13 14:49:19 +0100

    Form validation on login and register pages

--- Infoguagua/src/app/pages/login/login.ts
+++ Infoguagua/src/app/pages/login/login.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {form} from '@angular/forms/signals';
 import { PopupComponent } from '../../components/popup-form/popup-form';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, PopupComponent],
+  imports: [RouterLink, PopupComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  protected readonly form = form;
+  showPassword = false;
 
-  log() {
-    console.log('click recibido');
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', [
+        Validators.required
+      ]],
+      password: ['', [
+        Validators.required
+      ]]
+    });
   }
+
+  togglePassword(){ this.showPassword = !this.showPassword; }
+
+  onSubmit() {
+    if(this.form.valid) {
+      console.log("Formulario valido");
+    }
+  }
+
+  get username() { return this.form.get('username'); }
+  get password() { return this.form.get('password'); }
 }
--- Infoguagua/src/app/pages/register/register.ts
+++ Infoguagua/src/app/pages/register/register.ts
@@ -1,9 +1,63 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, AbstractControl, ValidationErrors, FormGroup} from '@angular/forms';
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if(password && confirmPassword && password.value !== confirmPassword.value) {
+    return { passwordMismatch: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+
+export class Register {
+  showPassword = false;
+  showPasswordConfirm = false;
+
+  form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+        Validators.pattern('^(?=.*[A-Za-z]).+$')
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(50),
+        Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d).+$')
+      ]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: passwordMatchValidator });
+  }
+
+
+
+  togglePassword(){ this.showPassword = !this.showPassword; }
+  togglePasswordConfirm(){ this.showPasswordConfirm = !this.showPasswordConfirm; }
+
+  onSubmit() {
+    if(this.form.valid) {
+      console.log("Formulario valido");
+    }
+  }
+
+  get username() { return this.form.get('username'); }
+  get email() { return this.form.get('email'); }
+  get password() { return this.form.get('password'); }
+  get confirmPassword() { return this.form.get('confirmPassword'); }
+}

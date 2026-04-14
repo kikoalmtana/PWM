fb4f40e672f99b4b10438215c1a126afa15e76b7
Andres Barroso <andresbarrososoriano@gmail.com>
2026-04-14 18:37:44 +0100

    add-pass page added

---
+++ Infoguagua/src/app/pages/add-pass/add-pass.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pass',
+  imports: [],
+  templateUrl: './add-pass.html',
+  styleUrl: './add-pass.css',
+})
+export class AddPass {}

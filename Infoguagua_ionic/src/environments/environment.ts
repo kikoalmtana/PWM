2fd0fd011aea46b52e5139b5c8ae33071e529600
kikoalmtana <kiko2005ulpgc@gmail.com>
2026-05-12 18:44:50 +0100

    login and register works!

--- Infoguagua_ionic/src/environments/environment.ts
+++ Infoguagua_ionic/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    authDomain: "infoguagua.firebaseapp.com",
-    projectId: "infoguagua",
-    storageBucket: "infoguagua.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:XXXXXXXXXXXXXXXXXXXXXX"
+    apiKey: "AIzaSyCYeSl1r7bCAuHIbnGT7Pgrq5BpQV_z3NA",
+    authDomain: "infoguaguas-eca00.firebaseapp.com",
+    projectId: "infoguaguas-eca00",
+    storageBucket: "infoguaguas-eca00.firebasestorage.app",
+    messagingSenderId: "590666024985",
+    appId: "1:590666024985:web:a06af2aaadb4bec77f4288"
   }
 };
